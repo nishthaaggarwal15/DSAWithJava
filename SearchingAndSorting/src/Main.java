4b6e97c1c8732bb6e8e0fbfa9dd1661cf150182d
Nishtha Aggarwal <145250198+nishthaaggarwal15@users.noreply.github.com>
2024-07-13 16:59:48 +0530

    updated

--- SearchingAndSorting/src/Main.java
+++ SearchingAndSorting/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is the mdodule of searching and sorting");
     }
 }
---
+++ SearchingAndSorting/src/bubblesort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class bubblesort {
+    public static void Sort (int arr[]){
+        int n = arr.length;
+        for(int i =0; i< n-1; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]= temp;
+                }
+            }
+        }
+        for(int i =0; i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the capacity of the array");
+        int cap = sc.nextInt();
+        int arr[]= new int[cap];
+        for( int i =0 ; i<cap; i++){
+            System.out.println("enter the value of " + i + "index");
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("given array is:");
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        System.out.println("sorted array is:");
+        Sort(arr);
+    }
+}
