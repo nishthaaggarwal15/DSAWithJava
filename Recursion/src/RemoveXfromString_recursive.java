d8f50ae49e27898dfe2d52a2489053c9452aec5a
Nishtha Aggarwal <145250198+nishthaaggarwal15@users.noreply.github.com>
2024-07-22 23:08:28 +0530

    updated

---
+++ Recursion/src/RemoveXfromString_recursive.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RemoveXfromString_recursive {
+    public static String removeX(String str){
+        if(str.isEmpty()){
+            return "";
+        }
+        char FirstChar = str.charAt(0);
+        String restOfString = removeX(str.substring(1));
+        if(FirstChar=='x'){
+            return  restOfString;
+        }
+        return FirstChar + restOfString;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string :");
+        String str = sc.nextLine();
+        System.out.println("updated String is : " + removeX(str));
+    }
+}
